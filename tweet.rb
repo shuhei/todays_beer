411cbefa5e0dbdc5882c33d2a7533e95ea7186ca
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2013-06-09 01:05:41 +0900

    Initial commit.

---
+++ tweet.rb
@@ -0,0 +1,14 @@
+require 'twitter'
+require 'dotenv'
+
+Dotenv.load
+
+Twitter.configure do |config|
+  config.consumer_key = ENV['CONSUMER_KEY']
+  config.consumer_secret = ENV['CONSUMER_SECRET']
+  config.oauth_token = ENV['OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['OAUTH_TOKEN_SECRET']
+end
+
+Twitter.update 'Hello, Beer!'
+
